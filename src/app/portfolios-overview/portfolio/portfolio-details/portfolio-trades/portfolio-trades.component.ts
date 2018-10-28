84b50e384f2b5100b6bc063f8e39252db61f646b
Joey73 <schmitz873@gmail.com>
2018-10-28 13:16:37 +0100

    Several improvements

--- src/app/portfolios-overview/portfolio/portfolio-details/portfolio-trades/portfolio-trades.component.ts
+++ src/app/portfolios-overview/portfolio/portfolio-details/portfolio-trades/portfolio-trades.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  payDate: string;
+  symbol: string;
+  buyOrSell: string;
+  amountOfShares: number;
+  pricePerShare: number;
+  priceTotal: number;
+  fee: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {payDate: '2018-06-07', symbol: 'TestSymbol1', buyOrSell: 'buy', amountOfShares: 10, pricePerShare: 123, priceTotal: 1234, fee: 10},
+  {payDate: '2018-09-28', symbol: 'TestSymbol2', buyOrSell: 'buy', amountOfShares: 20, pricePerShare: 321, priceTotal: 4321, fee: 10}
+];
+
 @Component({
   selector: 'app-portfolio-trades',
   templateUrl: './portfolio-trades.component.html',
   styleUrls: ['./portfolio-trades.component.sass']
 })
 export class PortfolioTradesComponent implements OnInit {
+  displayedColumns: string[] = ['payDate', 'symbol', 'buyOrSell', 'amountOfShares', 'pricePerShare', 'priceTotal', 'fee'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
